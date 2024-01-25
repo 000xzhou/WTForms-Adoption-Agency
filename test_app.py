66a349f099b68ceeb453438ba3030d6ae0221170
000xzhou <xzhou92@gmail.com>
2024-01-24 21:33:35 -0500

    added testing

--- test_app.py
+++ test_app.py
@@ -29,6 +29,7 @@ class UserFlaskCase(TestCase):
     def setUp(self):
         """Add sample"""
         with self.app.app_context():
+            Pet.query.delete()
             # Add a sample
             pet = Pet(name="Lucky", species="cat", available=False)
             db.session.add(pet)
@@ -52,34 +53,51 @@ class UserFlaskCase(TestCase):
     def test_pet_form_submit(self):
         with self.app.test_client() as client:
             resp = client.post("/pets/add", data={"name": "Star", "species": "dog"})
-            html = resp.get_data(as_text=True)
             self.assertEqual(resp.status_code, 302)
     # check to see if adding pet works and it's available on default
     def test_pet_form_submit_fwd_redirect(self):
         with self.app.test_client() as client:
             resp = client.post("/pets/add", data={"name": "Star", "species": "dog"}, follow_redirects=True)
             html = resp.get_data(as_text=True)
-
             self.assertEqual(resp.status_code, 200)
             self.assertIn("Star", html)
             self.assertIn("available", html)
-    # # checks to see if it return back to form if a value is Required 
+    # checks to see if it return back to form if a value is Required 
     def test_pet_form_submit_require_fail(self):
         with self.app.test_client() as client:
             resp = client.post("/pets/add", data={"name": "Star"})
             html = resp.get_data(as_text=True)
-
             self.assertEqual(resp.status_code, 200)
-            self.assertIn("form", html)
-    # # checks to see if it return back to form if a age is not between 0-30
+            self.assertIn('form id="add"', html)
+    # checks to see if it return back to form if a age is not between 0-30
     def test_pet_form_submit_age_fail(self):
         with self.app.test_client() as client:
             resp = client.post("/pets/add", data={"name": "Star","species": "dog", "age":100})
             html = resp.get_data(as_text=True)
-
             self.assertEqual(resp.status_code, 200)
             self.assertIn("Age must be between 0 and 30", html)
-            
+    # checks to see if it display the right form and if pet name and species is display
+    def test_pet_edit_form(self):
+        with self.app.test_client() as client:
+            resp = client.get(f"/pets/{self.pet_id}")
+            html = resp.get_data(as_text=True)
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('form id="edit"', html)
+            self.assertIn('Lucky', html)
+            self.assertIn('cat', html) 
+    # check to see if submiting works
+    def test_pet_edit_submit(self):
+        with self.app.test_client() as client:
+            resp = client.post(f"/pets/{self.pet_id}", data={"available": True})
+            self.assertEqual(resp.status_code, 302)
+    # check to see if submiting works follow up
+    def test_pet_edit_submit_fwd(self):
+        with self.app.test_client() as client:
+            resp = client.post(f"/pets/{self.pet_id}", data={"available": True}, follow_redirects=True)
+            html = resp.get_data(as_text=True)
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn("Lucky", html)
+            self.assertIn("available", html)
             
 if __name__ == "__main__":
     import unittest
